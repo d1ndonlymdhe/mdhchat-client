fbdf2cd761cb00b57eeaa86c7e1584343f492a65
Ayush <ayushshrestha8920@protnmail.com>
2021-09-27 21:08:37 +0545

    basic UI + functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,203 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useReducer, useEffect } from "react";
+import "./App.css";
+import io from "socket.io-client";
 
-function App() {
+const server = "192.168.100.23:4000";
+const socket = io(`http://localhost:4000`);
+
+export default function App() {
+  const [username, setUserName] = useState("");
+  const [roomCode, setRoomCode] = useState("");
+  const [roomCreated, setRoomCreated] = useState(false);
+  const [roomJoined, setRoomJoined] = useState(false);
+  useEffect(() => {
+    socket.on("roomCreated", (username, roomCode) => {
+      setRoomCreated(true);
+      setRoomJoined(false);
+      setRoomCode(roomCode);
+    });
+    socket.on("roomJoined", (username, roomCode) => {
+      console.log("joined");
+      setRoomCreated(false);
+      setRoomJoined(true);
+      setRoomCode(roomCode);
+    });
+    socket.on("error", (msg) => console.log(msg));
+  }, [socket]);
+
+  if (roomCreated || roomJoined) {
+    return (
+      <div id="chat">
+        <Title></Title>
+        <Chat socket={socket} username={username} roomCode={roomCode}></Chat>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <StartPage
+      socket={socket}
+      username={username}
+      setUserName={setUserName}
+      roomCode={roomCode}
+      setRoomCode={setRoomCode}
+    ></StartPage>
+  );
+}
+
+function StartPage({ socket, username, setUserName, roomCode, setRoomCode }) {
+  const handleSubmit = (e) => {
+    if (!(username == "username" || username == "")) {
+      e.preventDefault();
+      if (roomCode == 0 || roomCode == "") {
+        socket.emit("create", username);
+      } else {
+        console.log("join signal sent");
+        socket.emit("join", username, roomCode);
+      }
+    }
+  };
+  const isString = (str) => {
+    if (str.length > 4) {
+      return true;
+    }
+    const strArr = str.split("");
+    for (let i = 0; i < str.length; i++) {
+      if (
+        !["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(strArr[i])
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+  return (
+    <div id="mainContainer">
+      <Title></Title>
+      <br />
+      <div className="form">
+        <form onSubmit={handleSubmit} id="inputForm">
+          <input
+            type="text"
+            id="usernameInput"
+            autoComplete="off"
+            className="input"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            onFocus={(e) => {
+              e.target.value = "";
+            }}
+          />
+          <br />
+          <input
+            type="text"
+            id="roomCodeInput"
+            className="input"
+            autoComplete="off"
+            value={roomCode}
+            placeholder="Room Code"
+            onChange={(e) => {
+              if (isString(e.target.value)) {
+                return false;
+              }
+              setRoomCode(e.target.value);
+            }}
+            onFocus={(e) => (e.target.value = "")}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Chat({ socket, roomCode, username }) {
+  const string = `room code = ${roomCode}`;
+  console.log(roomCode);
+  const [messages, setMessages] = useState([
+    { from: username, msg: `${string}` },
+  ]);
+  // useEffect(() => {
+  //   setMessages([]);
+  // }, []);
+  const [msg, setMsg] = useState("");
+  useEffect(() => {
+    socket.on("msg", (res) => {
+      const { username, msg } = res;
+      console.log("recieved", username, msg);
+      const tempArr = messages;
+      tempArr.push({ from: username, msg: msg });
+      setMessages([...tempArr]);
+      console.log(messages);
+    });
+  }, [socket]);
+  const sendMsg = (e) => {
+    e.preventDefault();
+    if (msg != "") {
+      console.log("sent", username, msg);
+      socket.emit("msg", roomCode, msg, username);
+    }
+    setMsg("");
+  };
+
+  return (
+    <>
+      <Message messages={messages} username={username}></Message>
+      <form onSubmit={sendMsg}>
+        <Input msg={msg} setMsg={setMsg}></Input>
+
+        <button
+          className="btn"
+          style={{ width: "clamp(0px, 100%, 100%)" }}
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+    </>
+  );
+}
+
+function Message({ messages, username }) {
+  return (
+    <div id="messages">
+      {messages.map((message, index) => {
+        let sender = "";
+        if (message.from == username) {
+          sender = "You";
+        } else {
+          sender = message.from;
+        }
+        console.log(message.msg);
+        return (
+          <div className="msg" key={index}>{`${sender}: ${message.msg}`}</div>
+        );
+      })}
+    </div>
+  );
+}
+
+function Input({ msg, setMsg }) {
+  return (
+    <div>
+      <input
+        className="input"
+        value={msg}
+        onChange={(e) => setMsg(e.target.value)}
+        placeholder="Message"
+      ></input>
+      <AnyElem name="div"></AnyElem>
+    </div>
+  );
+}
+
+function Title() {
+  return <div class="title">MDHCHT</div>;
+}
+
+function AnyElem({ id, className, name }) {
+  return React.createElement(name);
+}
